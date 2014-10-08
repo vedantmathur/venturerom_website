ef320592d5e1bbb2d314def463481bba8f9809d5
Brett Bohnenkamper <kittykatt@kittykatt.us>
2014-10-08 02:51:09 -0500

    Initial commit of API, Dev, and WWW.

---
+++ api/index.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<title>VentureROM API</title>
+		<style>
+			@import url(http://fonts.googleapis.com/css?family=Roboto);
+			body { 
+				background-color: #0C0C0C;
+				font-family: Roboto, Helvetica;
+				color: #eee;
+			}
+			h1, h2 {
+				/* text-shadow: 0 1px 0 #ccc,
+					       0 2px 0 #c9c9c9,
+					       0 3px 0 #bbb,
+					       0 4px 0 #b9b9b9,
+					       0 5px 0 #aaa,
+					       0 6px 1px rgba(0,0,0,.1),
+					       0 0 5px rgba(0,0,0,.1),
+					       0 1px 3px rgba(0,0,0,.3),
+					       0 3px 5px rgba(0,0,0,.2),
+					       0 5px 10px rgba(0,0,0,.25),
+					       0 10px 10px rgba(0,0,0,.2),
+					       0 20px 20px rgba(0,0,0,.15); */
+				text-shadow: 0 -1px 0 #aaa;
+				color: #1483b9;
+			}
+			h3 { text-shadow: 0 -1px 0 #111; }
+			h1 {
+				font-size: 75px;
+			}
+			h2 {
+				font-size: 60px;
+			}
+			h3 {
+				font-size: 45px;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>VentureROM API</h1>
+		<h3>Valid API</h3>
+		<ul>
+			<li><b>/update/&lt;device name&gt;</b> - JSON output of updates for &lt;device name&gt;.</li>
+		</ul>
+	</body>
+</html>
