d3e2ecf3d56105f99a9d42d547eb9c9d56ea3377
KittyKatt <Brett Bohnenkamper>
2014-10-13 02:21:37 -0400

    Miscellanious stuff with navbar.

--- dev/index.php
+++ dev/index.php
@@ -111,6 +111,11 @@
 	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
 		<div class="container">
 			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-left navbar-main-collapse">
+				<ul class="nav navbar-nav">
+					<li><a class=page-scroll" href="http://dev.venturerom.com/">Developer Interface</a><li>
+				</ul>
+			</div>
 			<div class="collapse navbar-collapse navbar-right navbar-main-collapse">
 				<ul class="nav navbar-nav">
 					<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
